8ead153ca05d10d306aaaa8dade5774ab17090ce
thennys <thennyswhyte@gmail.com>
2024-08-12 16:42:40 -1200

    rollDice function implemented

--- main.py
+++ main.py
@@ -5,9 +5,27 @@ def rollDice(amount: int = 2) -> list[int]:
         raise ValueError
     
     rolls: list[int] = []
-    for i in range (1,6):
+    for i in range (amount):
         random_roll:int = random.randint(1, 6)
-        rolls.append(random_roll)
-    rolls.append(random_roll)
+        rolls.append(random_roll)  
 
     return rolls
+
+
+def main():
+    while True:
+        try:
+            userInput = input("How many dice woukd you like to roll? ")
+
+            if userInput.lower() == 'exit':
+                break
+
+            print(*rollDice (int(userInput)), sep=', ')
+
+        except ValueError:
+            print("Enter a valid number")
+
+
+if __name__ == '__main__':
+    main()
+
